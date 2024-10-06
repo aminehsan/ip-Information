5aa31c149340876a16770819a2ec5e228cb9cb65
Amin Ehsan <Mr.AminEhsan@gmail.com>
2024-10-06 11:27:28 +0330

    Refactoring Code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from time import sleep
-from requests import get
+import traceback
+import time
+import requests
 
 
 class IP:
@@ -8,18 +9,25 @@ class IP:
     __BASE_URL = 'http://ip-api.com/json'
     __PARAMS = {'fields': 'status,country,regionName,isp,mobile,proxy,hosting,query'}
 
-    def information(self, proxies: dict) -> dict:
+    def information(self, proxies: dict = None) -> dict:
         """Fetch IP information."""
         counter = 0
         while True:
             counter += 1
             try:
-                r = get(url=self.__BASE_URL, params=self.__PARAMS, proxies=proxies)
-                assert (r.status_code == 200) and ('application/json' in r.headers['Content-Type']) and (r.json()['status'] == 'success')
+                r = requests.get(url=self.__BASE_URL, params=self.__PARAMS, proxies=proxies)
+                assert (
+                        r.status_code == 200 and
+                        'application/json' in r.headers['Content-Type'] and
+                        r.json()['status'] == 'success'
+                ), 'Invalid response or failed status.'
                 return r.json()
             except Exception as e:
-                print(e)
+                print(f'Fetch IP information failed. Counter: {counter}')
+                traceback.print_exc()
                 if counter >= 5:
-                    print(f'Fetch IP information failed counter: {counter}')
                     raise e
-                sleep(10)
+                time.sleep(10)
+
+
+ip = IP()
